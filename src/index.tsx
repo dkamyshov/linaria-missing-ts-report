1d1cd20adb7d0444ddbb3ded4cc24310b1af20fc
Danil Kamyshov <danil.kamyshov@gmail.com>
2019-07-17 09:54:40 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { styled } from "linaria/react";
+import { styled as styled2 } from "linaria/lib/react";
+
+console.log(styled.div`
+  color: red;
+`);
+
+console.log(styled2.div`
+  color: blue;
+`);
